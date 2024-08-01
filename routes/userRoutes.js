98c407ce8567ddf94fa3a3546b87e2e039b4f0d2
pallavee-2705 <pallaveebisoyee@gmail.com>
2024-08-01 15:25:36 -0400

    Added user registration and login routes with JWT authentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,70 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+//try catch block for registration of user 
+//if user is registered successfully then the message will be sent
+router.post('/register', async (req, res) => {
+    try {
+    const { name, email, password} = req.body;
+
+    const user = new User({name, email, password});
+    await user.save();
+    res.status(201).send({ user, message:'User created successfully'});
+}
+
+catch (err){
+    res.status(400).send({error:err});
+}
+});
+
+//try catch block for login of user
+router.post('/login', async (req,res) => {
+    try{
+        const {email, password} = req.body;
+        //try to find if the user exists or not
+        const user = await User.findOne({ email });
+
+        //case to execute if user not found
+        if(!user){
+            throw new Error('Oops! Unable to login');
+        }
+    
+        //case if user is found
+        const isMatch = await bcrypt.compare(password, user.password);
+        
+        //if the password doesnt match
+        if(!isMatch){
+            throw new Error('Oops! Unable to login');
+        }
+
+        const token = jwt.sign({
+            _id: user._id.toString()            
+        } ,process.env.JWT_SECRET_KEY);
+
+        res.send({user, token, message:"Logged in successfully" });
+    }
+    catch (err) {
+        res.status(400).send({erro: err});
+    }
+});
 
 router.get('/', (req, res) =>{
-
     res.send('User routes are working');
 });
 
 
 //creating modules for login and register
+router.post('/register', async (req, res) => {
+    const{name, email, password} = req.body;
+});
+router.post('/login', async (req, res) =>{
 
+});
 //register the user
 
 //login the user
