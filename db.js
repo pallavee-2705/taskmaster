cbbd53a27bd9af11a5c67778d0b12836d740be5b
pallavee-2705 <pallaveebisoyee@gmail.com>
2024-08-01 10:37:36 -0400

    Set up MongoDB connection with Mongoose

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const MONGO_URL = process.env.MONGO_URL;
+const DB_NAME = process.env.DB_NAME;
+
+
+//defining a promise for resolve case and error case for connection to database
+mongoose.connect(MONGO_URL,{
+ dbName: DB_NAME
+}).then(() =>{
+    console.log('Yipee! Connected to the database successfully!');
+    }
+
+).catch((err) => {
+    console.log('Uh oh, failed to connect to database' + err);
+})
