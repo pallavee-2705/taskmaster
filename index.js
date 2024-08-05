57a2cd65ff3c2ffab7d115c4bf3cb9f0a96ea460
Pallavee Bisoyee <87165168+pallavee-2705@users.noreply.github.com>
2024-08-05 15:17:23 -0400

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-//defined an instance for express class to use it for api routes 
 const app = express();
 
-//define user routes
+//defining API routes
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
-
 
 require('dotenv').config();
 require('./db');
 const PORT = 8000;
 
-
 app.use(bodyParser.json());
 app.use('/users' , userRoutes);
 app.use('/tasks' , taskRoutes);
-
 
 app.get ('/',(req,res) => {
     res.json({
